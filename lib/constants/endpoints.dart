6f890a965d4d2c241285a4dd13092949fd1da1f9
Munibullah Shah <49975985+MunibullahShah@users.noreply.github.com>
2024-09-04 00:27:00 +0500

    Update endpoints.dart

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,11 +1,6 @@
 // EndPionts
-const baseUrl = "http://172.104.215.125:8100";
+const baseUrl = "http://google.com";
 
 const campaignsEndpoint = "api/agentCampaignList";
 const loginEndpoint = "api/loginAgents";
 const campaignDetailEndpoint = "api/campaignNumber";
-const tasksEndpoint = "api/tasks";
-const pauseCampaignEndpoint = "api/campaigns/updateStatus";
-const finalizedCallEndpoint = "api/updateCall";
-const callStatusEndpoint = "api/statuses";
-const timelineEndpoint = "api/timeline";
